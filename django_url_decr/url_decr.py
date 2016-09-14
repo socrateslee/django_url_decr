5c0f5d76306392628df53c2fc0d986fa0d367155
lichun <socrateslee@users.noreply.github.com>
2016-09-14 14:38:46 +0800

    Add a copy_url_component method, make sure a sub url set can be decorated for multiple times

--- django_url_decr/url_decr.py
+++ django_url_decr/url_decr.py
@@ -1,4 +1,5 @@
 import functools
+import copy
 from django.conf.urls import url
 try:
     from django.urls import RegexURLPattern, RegexURLResolver
@@ -6,7 +7,26 @@ except ImportError:
     from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
 
 
+def copy_url_component(source):
+    '''
+    Recursively copy RegexURLPattern and RegexURLResolver objects.
+    Since copy.deepcopy is unusable.
+    '''
+    curr = copy.copy(source)
+    if isinstance(curr, RegexURLPattern):
+        return curr
+    elif isinstance(curr, RegexURLResolver):
+        curr.url_patterns = map(copy_url_component, curr.url_patterns)
+        return curr
+    else:
+        raise Exception("Unknown url pattern type %s."\
+                        % repr(type(curr)))
+
+
 def dummy_decr(func):
+    '''
+    A dummy decorator
+    '''
     @functools.wraps(func)
     def wrap(*sub, **kw):
         print("Calling %s" % func.__name__)
@@ -36,6 +56,7 @@ def url_decr(*sub, **kw):
     decr = kw.pop('decr', None)
     pattern = url(*sub, **kw)
     if decr:
+        pattern = copy_url_component(pattern)
         for __, p in iter_pattern(pattern):
             # For django<1.10.1
             if hasattr(p, '_callback') and callable(p._callback):
